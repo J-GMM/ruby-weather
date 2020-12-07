2fa9592a0b9b1d11f27b6c3b8c90b8b2745acd37
Jose Martin <jgmm26@gmail.com>
2020-12-07 13:01:47 -0500

    Correcting style
    Updating style to properly connect CSS and html.erb

--- app/controllers/snapshot_controller.rb
+++ app/controllers/snapshot_controller.rb
@@ -5,7 +5,6 @@ require 'open-uri'
 class SnapshotController < ApplicationController
   def index
     # API Requests
-    # use try/catch blocks to display a request failed page
     ip = open('http://whatismyip.akamai.com').read
     locality_response = helpers.get_json("http://freegeoip.app/json/#{ip}")
     where_on_earth = helpers.get_json(
@@ -15,13 +14,13 @@ class SnapshotController < ApplicationController
     consolidated_weather = helpers.get_json(
       "https://www.metaweather.com/api/location/#{where_on_earth}")['consolidated_weather'][0]
 
-    # Location data for display
+    # Location
     @snapshot = Snapshot.new(snapshot_params)
     @snapshot.city = locality_response['city']
     @snapshot.region = locality_response['region_name']
     @snapshot.country = locality_response['country_code']
 
-    # Weather data
+    # Weather
     @snapshot.high_c = consolidated_weather['max_temp'].round(1)
     @snapshot.low_c = consolidated_weather['min_temp'].round(1)
     @snapshot.temp_c = consolidated_weather['the_temp'].round(1)
@@ -40,7 +39,6 @@ class SnapshotController < ApplicationController
     @weather_icon = "https://www.metaweather.com/static/img/weather/#{consolidated_weather['weather_state_abbr']}.svg"
     @created_dt = helpers.current_time(locality_response['time_zone'],
       consolidated_weather['created'])
-
     @display_class = helpers.get_color(@snapshot.temp_f)
     # dt = DateTime.strptime(consolidated_weather['created'].truncate(9), '%Y-%m-%dT%H:%M:%S')
   end # index
